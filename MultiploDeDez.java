e5160fd4d06f31b5e78b63fbaf32671ec467076c
Ju4nfp <112173691+Ju4nfp@users.noreply.github.com>
2022-12-01 22:52:43 -0300

    Create MultiploDeDez.java

---
+++ MultiploDeDez.java
@@ -0,0 +1,11 @@
+public class MultiplosDeDez{
+  public static void main (String[]args){
+    double res, i;
+    for(i = 1; i<=100;i++){
+      res = i%10;
+    if(res == 0) {
+      system.out.println("Multiplo de 10!"+i);
+      }
+    }
+  }
+}
